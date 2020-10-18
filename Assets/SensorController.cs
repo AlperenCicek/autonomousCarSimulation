d760b8696c5149d54d8464d139f81707ca90f8c6
Alperen Çiçek <alperencicek1998@gmail.com>
2020-10-18 23:59:32 +0300

    sensoraddition

---
+++ Assets/SensorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnTriggerStay(Collider col)
+	{
+		if(col.gameObject.tag == "Untagged")
+			Debug.Log("Çarptın");
+	}
+}
